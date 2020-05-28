2246067574fc288709ccf89cc187620fecc197c8
tuantv <vantuan@lubrytics.com>
2020-05-28 11:33:29 +0700

    update types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,4 +1,13 @@
-export interface YwnContext {
+/**
+ * @license
+ * Copyright tuantvk. All Rights Reserved.
+ * Licensed under the MIT License. See License in the project root for license information.
+ */
+
+import * as React from "react";
+
+
+export interface YwnContextProps {
   navigation?: any;
   route?: any;
   screens?: string[];
@@ -6,3 +15,9 @@ export interface YwnContext {
   debug?: boolean;
   children: React.ReactNode;
 }
+
+
+/** 
+Ywn.YwnContext
+*/
+export class YwnContext extends React.Component<YwnContextProps, any> { }
